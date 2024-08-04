ad5544bd08d80b8c3eced40fd17b11f6ebaf1921
NoulliCHEN <158829018+NoulliCHEN@users.noreply.github.com>
2024-08-04 15:20:12 +0800

    Update Cifar10_optimizer.py

--- data_classification/Cifar10_optimizer.py
+++ data_classification/Cifar10_optimizer.py
@@ -10,11 +10,26 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import os
 from utils import Logger, AverageMeter, accuracy, mkdir_p, savefig
+import time
 
 from PIDAO_SI import PIDAccOptimizer_SI
 from PIDAO_Sym_Tz import PIDAccOptimizer_Sym_Tz
 from PIDAO_SI_AAdRMS import PIDAccOptimizer_SI_AAdRMS
+from PIDopt import PIDOptimizer
+from AdaHB import Adaptive_HB
 
+import argparse
+# args configurationz
+parser = argparse.ArgumentParser(description='FashionMNIST Example Config')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+parser.add_argument('--ki', type=float, default=0.3, help='ki')
+parser.add_argument('--kd', type=float, default=1, help='kd')
+parser.add_argument('--gpu', type=int, default=0, help='the number of gpu for training')
+parser.add_argument('--batch-size', type=int, default=128, help='batch size')
+parser.add_argument('--num-epochs', type=int, default=140, help='number of epochs to train')
+parser.add_argument('--model', type=str, default='ResNet18', help='model type')
+args = parser.parse_args()
 
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
@@ -41,7 +56,7 @@ test_data = datasets.CIFAR10(root='./data',
                              )
 
 # Data Loader (Input Pipeline)
-batch_size = 128
+batch_size = args.batch_size
 train_loader = DataLoader(dataset=training_data,
                           batch_size=batch_size,
                           shuffle=True,
@@ -55,7 +70,7 @@ test_loader = DataLoader(dataset=test_data,
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-num_epochs = 150
+num_epochs = args.num_epochs
 
 
 # Neural Network Model
@@ -111,7 +126,7 @@ def train_loop(train_data, model, loss_fn, optimizer, epoch, device, scheduler):
 
         if (batch + 1) % 200 == 0:
             print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Acc: %.8f'
-                  % (epoch + 1, num_epochs, batch + 1, len(train_data), train_loss_log.avg,
+                  % (epoch + 1, args.num_epochs, batch + 1, len(train_data), train_loss_log.avg,
                      train_acc_log.avg))
     scheduler.step()
 
@@ -154,7 +169,7 @@ def Optimizers(model, method_name, learning_rate):
     lr = learning_rate
     alr = learning_rate/10  # for adaptive optimizers
     # FNN
-    equivalent_momentum = 0.9
+    equivalent_momentum = args.momentum
     momentum = (1 / equivalent_momentum - 1) / lr
     ki = 0.3
     kd = 1
@@ -163,7 +178,7 @@ def Optimizers(model, method_name, learning_rate):
     p_ar_lr = alr
     momentum_ar = (1 / equivalent_momentum - 1) / p_ar_lr
     ki_ar = 0.1
-    kd_ar = 1
+    kd_ar = 1*args.kd
     kp_ar = 1 * p_ar_lr * (1 + momentum_ar * p_ar_lr) / p_ar_lr ** 2
     # a collection of all optimizers for comparisons
     optimizers = {
@@ -175,6 +190,10 @@ def Optimizers(model, method_name, learning_rate):
                                                weight_decay=0.0001, momentum=momentum, kp=kp, ki=ki, kd=kd),
         'PIDAO_SI': PIDAccOptimizer_SI(model.parameters(), lr=lr, weight_decay=0.0001, momentum=momentum, kp=kp, ki=ki, kd=kd),
         'PIDAO_SI_AAdRMS': PIDAccOptimizer_SI_AAdRMS(model.parameters(), lr=p_ar_lr, weight_decay=0.0001, momentum=momentum_ar, kp=kp_ar, ki=ki_ar, kd=kd_ar),
+        'PIDopt': PIDOptimizer(model.parameters(), lr=lr, weight_decay=0.0001,
+                                                   momentum=equivalent_momentum, I=1, D=100),
+        'AdaHB': Adaptive_HB(model.parameters(), lr=alr, weight_decay=0.0001, momentum_init=0.9),
+
     }
     return optimizers[method_name]
 
@@ -182,6 +201,7 @@ def Optimizers(model, method_name, learning_rate):
 def main(train_data, test_data, model, loss_fn, optimizer, num_epochs, logger, device, scheduler):
     for epoch in range(num_epochs):
         print(f"Epoch {epoch + 1}\n-------------------------------")
+        start_time = time.time()
         train_loss_log, train_acc_log = train_loop(train_data=train_data,
                                                    model=model,
                                                    loss_fn=loss_fn,
@@ -189,6 +209,8 @@ def main(train_data, test_data, model, loss_fn, optimizer, num_epochs, logger, d
                                                    epoch=epoch,
                                                    device=device,
                                                    scheduler=scheduler)
+        end_time = time.time()
+        execution_time =  round(end_time - start_time, 3)
         val_loss_log, val_acc_log = test_loop(test_data=test_data,
                                               model=model,
                                               loss_fn=loss_fn,
@@ -197,13 +219,14 @@ def main(train_data, test_data, model, loss_fn, optimizer, num_epochs, logger, d
                        train_loss_log.avg,
                        val_loss_log.avg,
                        train_acc_log.avg,
-                       val_acc_log.avg])
+                       val_acc_log.avg, 
+                       execution_time])
     logger.close()
     logger.plot()
 
 
 if __name__ == '__main__':
-    learning_rate = 0.01
+    learning_rate = args.lr
     path = 'results/Cifar10' + '/learning_rate={0}'.format(learning_rate)
     folder = os.path.exists(path)
     if not folder:
@@ -215,7 +238,7 @@ if __name__ == '__main__':
               'ResNet34': ResNet34(),
               'VGG16': VGG('VGG16'),
               'SimpleDLA': SimpleDLA()}
-    NN = 'ResNet18'
+    NN = args.model
     initial_net = NN_set[NN]
     path_nn = path + '/NN={0}'.format(NN)
     if not os.path.exists(path_nn):
@@ -223,18 +246,25 @@ if __name__ == '__main__':
     # save the net's structure
     torch.save(initial_net, path_nn + '/initial_net.pkl')
 
-    method = ['PIDAO_Sym_Tz', 'SGDM', 'PIDAO_SI', 'Adam', 'PIDAO_SI_AAdRMS']
+    method = [
+        'PIDAO_SI', 
+        'SGDM', 
+        'PIDopt',
+        'PIDAO_Sym_Tz',
+        'Adam',
+        'PIDAO_SI_AAdRMS',
+        'AdaHB'
+        ]
     for optimizer_name in method:
         logger = Logger(path_nn + '/' + optimizer_name + '.txt')
-        logger.set_names(['Learning Rate', 'Train Loss', 'Valid Loss', 'Train Acc.', 'Valid Acc.'])
+        logger.set_names(['Learning Rate', 'Train Loss', 'Valid Loss', 'Train Acc.', 'Valid Acc.', 'Time'])
         net = torch.load(path_nn + '/initial_net.pkl')
         # GPU or CPU
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
         net.to(device)
         loss = nn.CrossEntropyLoss()
         opt = Optimizers(net, optimizer_name, learning_rate)
-        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer=opt, step_size=100, gamma=.5)
-        # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=opt, milestones=[50,150,200], gamma=0.5, last_epoch=-1)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer=opt, step_size=50, gamma=1)
 
         print('Here is a training process driven by the {0} optimizer'.format(optimizer_name))
         main(train_data=train_loader,
@@ -242,7 +272,7 @@ if __name__ == '__main__':
              model=net,
              loss_fn=loss,
              optimizer=opt,
-             num_epochs=num_epochs,
+             num_epochs=args.num_epochs,
              logger=logger,
              device=device,
              scheduler=scheduler
